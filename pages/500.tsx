4f39ba1ca0334791c487684cb618381e847ae2db
Geno Ferollo <genemators@gmail.com>
2020-12-18 16:59:14 +0500

    fix error page

--- pages/500.tsx
+++ pages/500.tsx
@@ -3,8 +3,6 @@
 import Head from "next/head";
 import React from "react";
 import Router from "next/router";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 export default function Custom404() {
   return (
@@ -17,7 +15,6 @@ export default function Custom404() {
           content="This page's preview is not available as this page doesn't exist"
         />
       </Head>
-      <Header subtitle="404" />
       <div className="h-auto">
         <div className="flex mb-4 justify-center mt-64">
           <div className="text-white text-center text-center xl:text-6xl lg:text-5xl text-4xl border rounded hover:bg-white hover:text-black">
@@ -39,7 +36,6 @@ export default function Custom404() {
           </a>
         </div>
       </div>
-      <Footer />
     </>
   );
 }
