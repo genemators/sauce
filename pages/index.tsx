6dd90921a3c8cc6bb43210a37cd22a69037e9358
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-02-16 19:22:17 +0500

    yes

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,13 @@ import React from "react";
 import Head from "next/head";
 import { GetStaticProps } from "next/types";
 
-import { promises } from "fs";
+interface Props {
+  props: {
+    message: string;
+  };
+}
 
-const IndexPage = (props: any): React.ReactElement => {
+const IndexPage = (props: Props): React.ReactElement => {
   return (
     <>
       <Head>
@@ -32,31 +36,27 @@ const IndexPage = (props: any): React.ReactElement => {
             </div>
           </div>
           <div className="mt-6 grid gap-16 lg:grid-cols-2 lg:col-gap-5 lg:row-gap-12">
-            {props.dir.map((content: any) => {
-              return (
-                <div key={content}>
-                  <a
-                    href={"https://sauce.genemator.me/" + content}
-                    className="block"
-                  >
-                    <h3 className="mt-2 text-xl leading-7 font-semibold text-white">
-                      {content}
-                    </h3>
-                    <p className="mt-3 text-base leading-6 text-gray-300">
-                      Press the button below to open it!
-                    </p>
-                  </a>
-                  <div className="mt-3">
-                    <a
-                      href={"https://sauce.genemator.me/" + content}
-                      className="read-post text-base leading-6 font-semibold transition ease-in-out duration-150"
-                    >
-                      Download
-                    </a>
-                  </div>
-                </div>
-              );
-            })}
+            <div key={props.props.message}>
+              <a
+                href={"https://sauce.genemator.me/" + props.props.message}
+                className="block"
+              >
+                <h3 className="mt-2 text-xl leading-7 font-semibold text-white">
+                  {props.props.message}
+                </h3>
+                <p className="mt-3 text-base leading-6 text-gray-300">
+                  Press the button below to open it!
+                </p>
+              </a>
+              <div className="mt-3">
+                <a
+                  href={"https://sauce.genemator.me/" + props.props.message}
+                  className="read-post text-base leading-6 font-semibold transition ease-in-out duration-150"
+                >
+                  Download
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -65,9 +65,10 @@ const IndexPage = (props: any): React.ReactElement => {
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const dir = await promises.readdir("./public");
   return {
-    props: { dir },
+    props: {
+      message: "Hello World",
+    },
   };
 };
 
